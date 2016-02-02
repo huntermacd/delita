f0f97c21fb1670ea35df77c99ef4e04204c87b4e
Hunter MacDermut <whosebluesanyway@gmail.com>
2016-02-02 17:32:29 -0500

    Init commit, file setup

---
+++ game.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(700, 700, '', Phaser.CANVAS, {preload: preload, create: create, update: update});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
