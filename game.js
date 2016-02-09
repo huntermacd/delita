6bc5d35aca81b1ced748c4fb62efae65e11f502e
Hunter MacDermut <whosebluesanyway@gmail.com>
2016-02-09 13:28:17 -0500

    Add knight hit functionality

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(700, 500, '', Phaser.CANVAS, {preload: preload, create: create, update: update, render: render});
 
-var acting_sprite;
+var acting_sprite,
+    target_sprite;
 
 function preload(){
     game.load.image('blue_archer', 'sprites/blue_archer.png');
@@ -17,16 +18,21 @@ function preload(){
     game.load.image('terrain_5', 'sprites/terrain_5.png');
     game.load.image('terrain_6', 'sprites/terrain_6.png');
     game.load.spritesheet('move_target', 'sprites/move_target.png', 100, 100, 5);
+    game.load.spritesheet('hit_target', 'sprites/hit_target.png', 100, 100, 5);
 }
 
 function create(){
+    Phaser.Sprite.prototype.is_terrain = false;
+    Phaser.Sprite.prototype.team = 'blue';
+
     game.add.sprite(0, 0, 'board');
 
     move_targets = game.add.group();
+    hit_targets = game.add.group();
 
     // "extended sprite"
-    MoveTarget = function(game, x, y){
-        Phaser.Sprite.call(this, game, x, y, 'move_target');
+    Target = function(game, x, y, key){
+        Phaser.Sprite.call(this, game, x, y, key);
 
         this.animations.add('blink');
         this.animations.play('blink', 8, true);
@@ -34,24 +40,51 @@ function create(){
 
         this.inputEnabled = true;
         this.input.useHandCursor = true;
+        // this.events.onInputDown.add(submit_move, this);
+    }
+
+    Target.prototype = Object.create(Phaser.Sprite.prototype);
+    Target.prototype.constructor = Target;
+
+    MoveTarget = function(game, x, y, key){
+        Target.call(this, game, x, y, key);
+
         this.events.onInputDown.add(submit_move, this);
     }
 
-    MoveTarget.prototype = Object.create(Phaser.Sprite.prototype);
+    MoveTarget.prototype = Object.create(Target.prototype);
     MoveTarget.prototype.constructor = MoveTarget;
 
+    HitTarget = function(game, x, y, key){
+        Target.call(this, game, x, y, key);
+
+        this.events.onInputDown.add(submit_hit, this);
+    }
+
+    HitTarget.prototype = Object.create(Target.prototype);
+    HitTarget.prototype.constructor = HitTarget;
+
     for (var i = 0; i < 8; i++) {
-        move_target = new MoveTarget(game, 0, 0);
+        move_target = new MoveTarget(game, 0, 0, 'move_target');
         move_targets.add(game.add.existing(move_target));
         move_targets.setAll('exists', false);
+    };
+
+    for (var i = 0; i < 3; i++) {
+        hit_target = new HitTarget(game, 0, 0, 'hit_target');
+        hit_targets.add(game.add.existing(hit_target));
+        hit_targets.setAll('exists', false);
     };
 
     blue_archer = game.add.sprite(150, 50, 'blue_archer');
     blue_knight = game.add.sprite(350, 50, 'blue_knight');
     blue_mage = game.add.sprite(550, 50, 'blue_mage');
     red_archer = game.add.sprite(550, 450, 'red_archer');
+    red_archer.team = 'red';
     red_knight = game.add.sprite(350, 450, 'red_knight');
+    red_knight.team = 'red';
     red_mage = game.add.sprite(150, 450, 'red_mage');
+    red_mage.team = 'red';
 
     all_units = [
         blue_archer,
@@ -86,6 +119,7 @@ function create(){
 
     for (var i = 0; i < all_terrain.length; i++) {
         all_terrain[i].anchor.setTo(0.5);
+        all_terrain[i].is_terrain = true;
     };
 
     all_sprites = all_units.concat(all_terrain);
@@ -115,6 +149,24 @@ function occupied(x, y){
     return false;
 }
 
+function in_bounds(x, y){
+    if (x >= 0 && x <= game.world.width && y >= 0 && y <= game.world.height){
+        return true;
+    }
+
+    return false;
+}
+
+function sprite_from_point(x, y){
+    for (var i = 0; i < all_sprites.length; i++) {
+        if (all_sprites[i].x === x && all_sprites[i].y === y){
+            return all_sprites[i];
+        }
+    }
+
+    return undefined;
+}
+
 function move(unit){
     var n = [unit.x, unit.y - 100];
     var e = [unit.x + 100, unit.y];
@@ -123,19 +175,19 @@ function move(unit){
 
     var valid_tiles = [];
 
-    if (n[0] >= 0 && n[0] <= game.world.width && n[1] >= 0 && n[1] <= game.world.height && !occupied(n[0], n[1])){
+    if (in_bounds(n[0], n[1]) && !occupied(n[0], n[1])){
         valid_tiles.push(n);
     }
 
-    if (e[0] >= 0 && e[0] <= game.world.width && e[1] >= 0 && e[1] <= game.world.height && !occupied(e[0], e[1])){
+    if (in_bounds(e[0], e[1]) && !occupied(e[0], e[1])){
         valid_tiles.push(e);
     }
 
-    if (s[0] >= 0 && s[0] <= game.world.width && s[1] >= 0 && s[1] <= game.world.height && !occupied(s[0], s[1])){
+    if (in_bounds(s[0], s[1]) && !occupied(s[0], s[1])){
         valid_tiles.push(s);
     }
 
-    if (w[0] >= 0 && w[0] <= game.world.width && w[1] >= 0 && w[1] <= game.world.height && !occupied(w[0], w[1])){
+    if (in_bounds(w[0], w[1]) && !occupied(w[0], w[1])){
         valid_tiles.push(w);
     }
 
@@ -154,4 +206,51 @@ function submit_move(){
     acting_sprite.y = this.y;
 
     move_targets.setAll('exists', false);
+}
+
+function knight_attack(unit){
+    var n = [unit.x, unit.y - 100];
+    var e = [unit.x + 100, unit.y];
+    var s = [unit.x, unit.y + 100];
+    var w = [unit.x - 100, unit.y];
+
+    var n_sprite = sprite_from_point(n[0], n[1]);
+    var e_sprite = sprite_from_point(e[0], e[1]);
+    var s_sprite = sprite_from_point(s[0], s[1]);
+    var w_sprite = sprite_from_point(w[0], w[1]);
+
+    var valid_tiles = [];
+
+    acting_sprite = unit;
+
+    if (in_bounds(n[0], n[1]) && occupied(n[0], n[1]) && !n_sprite.is_terrain && n_sprite.team !== acting_sprite.team){
+        valid_tiles.push(n);
+    }
+
+    if (in_bounds(e[0], e[1]) && occupied(e[0], e[1]) && !e_sprite.is_terrain && e_sprite.team !== acting_sprite.team){
+        valid_tiles.push(e);
+    }
+
+    if (in_bounds(s[0], s[1]) && occupied(s[0], s[1]) && !s_sprite.is_terrain && s_sprite.team !== acting_sprite.team){
+        valid_tiles.push(s);
+    }
+
+    if (in_bounds(w[0], w[1]) && occupied(w[0], w[1]) && !w_sprite.is_terrain && w_sprite.team !== acting_sprite.team){
+        valid_tiles.push(w);
+    }
+
+    add_hit_sprites(valid_tiles);
+}
+
+function add_hit_sprites(tiles){
+    for (var i = 0; i < tiles.length; i++) {
+        hit_targets.getFirstExists(false, false, tiles[i][0], tiles[i][1]);
+    };
+}
+
+function submit_hit(){
+    target_sprite = sprite_from_point(this.x, this.y);
+    target_sprite.health--;
+
+    hit_targets.setAll('exists', false);
 }
